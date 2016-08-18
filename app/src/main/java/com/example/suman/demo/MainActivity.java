ed2e8e6352f06f6ee510fd3195d7dcd896c35748
Suman <sumamyon.am@outlook.com>
2016-08-18 22:08:19 +0545

    MainActivity

--- app/src/main/java/com/example/suman/demo/MainActivity.java
+++ app/src/main/java/com/example/suman/demo/MainActivity.java
@@ -2,12 +2,48 @@ package com.example.suman.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText e1,e2;
+    Button b;
+    TextView t;
+    private float sum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        e1 = (EditText)findViewById(R.id.editText);
+        e2 = (EditText)findViewById(R.id.editText2);
+        b = (Button)findViewById(R.id.button);
+        t = (TextView)findViewById(R.id.textView3);
+
+        AddData();
     }
+
+    public  void AddData(){
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float n1,n2,sum;
+                n1 = Float.parseFloat(e1.getText().toString());
+                n2 = Float.parseFloat(e2.getText().toString());
+
+                sum = n1 + n2;
+
+                s(sum);
+            }
+        });
+    }
+
+    private void s(float sum) {
+        this.sum = sum;
+
+        t.setText("SUM : "+sum);
+    }
+
 }
